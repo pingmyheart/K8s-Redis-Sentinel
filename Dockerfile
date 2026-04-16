ce73b11fb266414bbf5e4a44cd4be5306ee62aa9
GitlabCiCd <antonio.russi.consulting@gmail.com>
2026-04-16 11:54:15 +0200

    [ADD] - init

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM redis:8.2.5
+RUN apk add --no-cache curl jq
---
+++ setup.py
@@ -0,0 +1,12 @@
+import warnings
+
+from setuptools import setup, find_packages
+
+# Suppress all warnings
+warnings.filterwarnings("ignore")
+
+setup(
+    name='redis',
+    version='8.2.5.dev0',
+    packages=find_packages()
+)
